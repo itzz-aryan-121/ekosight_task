bf16dc3a7db001eca17e996c6471d431e9c9aa76
Aryan Tomar <122987294+itzz-aryan-121@users.noreply.github.com>
2025-11-20 14:07:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,15 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://ekosight-task-xwd2.vercel.app'
+        'https://ekosight-task-xwd2.vercel.app',
+        'https://ekosight-task-3.onrender.com'
     ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }))
+
+app.options('*', cors());
 
 
 
